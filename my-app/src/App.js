185b83bb7f5b242c566758f5f017f77e535f26c7
Your name here <brkmer@gmail.com>
2022-12-01 16:17:21 -0500

    DigitButton replaced button for numbers

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -23,7 +23,7 @@ function reducer(state, {type, payload}) {
 function App() {
   const [{currentOperand,previousOperand, operation}, dispatch] = useReducer(reducer, {})
 
-  dispatch({type: ACTIONS.ADD_DIGIT, payload: {digit:1 }})
+  
   return (
     <div className="calculator-grid">
       <div className="output">
@@ -31,22 +31,20 @@ function App() {
         <div className="current-operand">{currentOperand}</div>
       </div>
       <button className="span-two">AC</button>
-      <button >DEL</button>
-      <button >÷</button>
-      <button >1</button>
-      <button >2</button>
-      <button >3</button>
-      <button >*</button>
-      <button >4</button>
-      <button >5</button>
-      <button >6</button>
-      <button >+</button>
-      <button >7</button>
-      <button >8</button>
-      <button >9</button>
-      <button >-</button>
-      <button >.</button>
-      <button >0</button>
+      <button>DEL</button>
+      <DigitButton digit="÷" dispatch={dispatch}/>
+      <DigitButton digit="1" dispatch={dispatch}/>
+      <DigitButton digit="2" dispatch={dispatch}/>
+      <DigitButton digit="3" dispatch={dispatch}/>
+      <DigitButton digit="4" dispatch={dispatch}/>
+      <DigitButton digit="5" dispatch={dispatch}/>
+      <DigitButton digit="6" dispatch={dispatch}/>
+      <DigitButton digit="7" dispatch={dispatch}/>
+      <DigitButton digit="8" dispatch={dispatch}/>
+      <DigitButton digit="9" dispatch={dispatch}/>
+      <DigitButton digit="." dispatch={dispatch}/>
+      <DigitButton digit="0" dispatch={dispatch}/>
+
       <button className="span-two">=</button>
     
     </div>
